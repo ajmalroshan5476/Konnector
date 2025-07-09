8984b35f38423a9e82e93a8b3d4f36db8c894709
ajmalroshan5476 <ajmalroshanav5590@gmail.com>
2025-07-09 22:59:25 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <body className='m-0 p-0'>
+    <div className='min-h-screen bg-primarybg'>
+      <h1>Hii</h1>
+    </div>
+    </body>
+  )
+}
+
+export default App
+
