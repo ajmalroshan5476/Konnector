eb258067a1c32d00a326c7a135c00198f94cb883
ajmalroshan5476 <ajmalroshanav5590@gmail.com>
2025-07-15 03:37:34 +0530

    Your commit message

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import LogoTitle from '../components/LogoTitle'
 
 const Home = () => {
